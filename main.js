92e4fc809bacbf6536ab609cebc22e3d693d4b4e
Ignacio Grane <graneignacio@gmail.com>
2024-06-04 12:17:09 -0300

    Realizamos los ejercicios hasta el inciso 7

--- main.js
+++ main.js
@@ -1,3 +1,65 @@
 import './css/style.css';
 
+// Inciso 1
 const nombre = 'Ignacio';
+
+// Inciso 2
+
+let edad = 27;
+let precio = 165.75;
+let ciudad = 'La Plata';
+let esEstudiante = true;
+
+// Inciso 3
+
+const nombre2 = 'Martin';
+const apellido = 'Gomez';
+console.log(`Hola, ${nombre2} ${apellido}.`);
+
+// Inciso 4
+
+const num1 = 10;
+const num2 = 50;
+
+// ! Suma
+
+const Suma = num1 + num2;
+console.log(`El resultado de la suma de ${num1} y ${num2} es ${Suma}`);
+
+// ! Resta
+
+const Resta = num1 - num2;
+console.log(`El resultado de la resta entre ${num1} y ${num2} es ${Resta}`);
+
+// ! Multiplicación
+
+const multiplicacion = num1 * num2;
+console.log(
+  `El resultado de la multiplicación entre ${num1} y ${num2} es ${multiplicacion}`,
+);
+
+// ! División
+
+const division = num1 / num2;
+console.log(
+  `El resultado de la division entre ${num1} y ${num2} es ${division}`,
+);
+
+// Inciso 5
+
+let contador = 0;
+
+contador++; // Esta en 0 y debe dar 1.
+
+console.log(contador);
+
+contador--; //Esta en 1 y debe dar 0.
+
+console.log(contador);
+
+// Inciso 6
+
+const nombreUsuario = prompt('Ingrese un nombre de usuario', '');
+console.log(`El nombre del usuario ingresado es ${nombreUsuario}`);
+
+// Inciso 7
