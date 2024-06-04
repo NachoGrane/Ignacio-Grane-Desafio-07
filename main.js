06a3d82ee5bb61653f927e5ee320f8758cffb722
Ignacio Grane <graneignacio@gmail.com>
2024-06-04 13:32:43 -0300

    terminamos los 12 incisos

--- main.js
+++ main.js
@@ -59,7 +59,83 @@ console.log(contador);
 
 // Inciso 6
 
-const nombreUsuario = prompt('Ingrese un nombre de usuario', '');
-console.log(`El nombre del usuario ingresado es ${nombreUsuario}`);
+// const nombreUsuario = prompt('Ingrese un nombre de usuario', '');
+// console.log(`El nombre del usuario ingresado es ${nombreUsuario}`);
 
 // Inciso 7
+
+// const radio = prompt('Ingrese el radio');
+// const pi = 3.14;
+// const calculoArea = pi * (radio * radio);
+
+// console.log(`El área es igual a ${calculoArea}`);
+
+// Inciso 8
+
+// const gradosCelsius = prompt('Ingrese los grados celsius');
+// const gradosFahrenheit = (9 / 5) * gradosCelsius + 32;
+
+// console.log(
+//   `Los grados celsius ${gradosCelsius} corresponden a ${gradosFahrenheit} grados fahrenheit`,
+// );
+
+// Inciso 9
+
+// let cantNum = 0;
+// const numero1 = prompt('Ingrese el número 1');
+// cantNum++;
+// const numero2 = prompt('Ingrese el número 2');
+// cantNum++;
+// const numero3 = prompt('Ingrese el número 3');
+// cantNum++;
+// const calculoPromedio =
+//   (Number(numero1) + Number(numero2) + Number(numero3)) / cantNum;
+// console.log(
+//   `El calculo de promedio entre los 3 numeros ingresados es de ${calculoPromedio}`,
+// );
+
+// Inciso 10
+
+const numero = 10;
+
+console.log(numero >= 0 ? 'Es positivo' : 'Es negativo');
+
+// Inciso 11
+
+// const edadUsuario = prompt('Ingrese su edad');
+
+// if (edadUsuario >= 18) {
+//   console.log('Es mayor de edad');
+// } else console.log('Es menor de edad');
+
+// Inciso 12
+
+// let diaSemana = prompt(
+//   'Ingrese un día de la semana \n 1. lunes \n 2. martes \n 3. miércoles \n 4. jueves \n 5. viernes \n 6. sábado \n 7. domingo',
+// );
+// switch (diaSemana) {
+//   case '1':
+//     console.log('El día de la semana seleccionado es el lunes');
+//     break;
+//   case '2':
+//     console.log('El día de la semana seleccionado es el martes');
+//     break;
+//   case '3':
+//     console.log('El día de la semana seleccionado es el miércoles');
+//     break;
+//   case '4':
+//     console.log('El día de la semana seleccionado es el jueves');
+//     break;
+//   case '5':
+//     console.log('El día de la semana seleccionado es el viernes');
+//     break;
+//   case '6':
+//     console.log('El día de la semana seleccionado es el sábado');
+//     break;
+//   case '7':
+//     console.log('El día de la semana seleccionado es el domingo');
+//     break;
+//   default:
+//     console.log('Número de día de semana inexistente');
+//     break;
+// }
